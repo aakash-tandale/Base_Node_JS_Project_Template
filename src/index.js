a2fa14c4238176f251da9a43544920087dbc1f45
Aakash Tandale <aakashtandale@Aakashs-Air.lan>
2025-04-05 20:44:26 +0530

    initial backend code setup for node

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+const {PORT} = require('./config');
+const express = require('express');
+
+const app=express();
+app.listen(PORT, () => {
+    console.log(`Server is running on ports ${PORT}`);
+}
+);
